073297ec615fbd2642e42e3a3c0e8a54b94362a9
Peter Yao <peyao@protonmail.ch>
2024-02-26 15:38:31 -0800

    Restore <C-space> incremental selection

--- init.lua
+++ init.lua
@@ -809,6 +809,13 @@ require('lazy').setup({
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-Space>',
+            node_incremental = '<C-Space>',
+          },
+        },
       }
 
       -- There are additional nvim-treesitter modules that you can use to interact

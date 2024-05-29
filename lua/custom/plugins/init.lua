c2b5a8b692e5587c2b1ce163b5b78b0de4434313
Peter Yao <peyao@protonmail.ch>
2024-05-29 12:10:36 -0700

    Change nvim-ts-autotag config to use require('...').setup

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,11 +7,15 @@ return {
   { 'terrortylor/nvim-comment' }, -- add commenter
   {
     'windwp/nvim-ts-autotag',
-    opts = {
-      enable_close = true, -- Auto close tags
-      enable_rename = true, -- Auto rename pairs of tags
-      enable_close_on_slash = false -- Auto close on trailing </
-    }
+    config = function()
+      require('nvim-ts-autotag').setup {
+        opts = {
+          enable_close = true, -- Auto close tags
+          enable_rename = true, -- Auto rename pairs of tags
+          enable_close_on_slash = false -- Auto close on trailing </
+        }
+      }
+    end,
   }, -- open/close html/jsx tags together
   {
     'dstein64/nvim-scrollview',

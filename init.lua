5186bf7bb88a635829c0133d484cf03cc57b8b3a
Peter Yao <peyao@protonmail.ch>
2024-07-24 11:53:17 -0700

    Add auto-window resize when terminal resizes, which-key changes

--- init.lua
+++ init.lua
@@ -167,6 +167,13 @@ vim.opt.sessionoptions = 'curdir,folds,globals,help,tabpages,terminal,winsize' -
 vim.opt.mousescroll = 'ver:1,hor:1'
 vim.opt.mousemoveevent = true
 
+-- Automatically resize opened windows when terminal size changes
+vim.opt.equalalways = true
+vim.api.nvim_create_autocmd('VimResized', {
+  pattern = '*',
+  command = 'wincmd = '
+})
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -289,26 +296,35 @@ require('lazy').setup({
 
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter' or 'VeryLazy'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      require('which-key').setup({
+        preset = "helix",
+        -- modes = {
+        --   x = false,
+        -- },
+        triggers = {
+          { "<auto>", mode = "nisotc" }, -- default: "nixsotc" (x: visual mode)
+        },
+        win = {
+          -- height = { max = 10 },
+          wo = {
+            winblend = 20
+          }
+        }
+      })
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>l'] = { name = '[L]og', _ = 'which_key_ignore' },
-        ['<leader>T'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>G'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { "<leader>G", group = "[G]it" },
+        { "<leader>T", group = "[T]oggle" },
+        { "<leader>c", group = "[C]ode" },
+        { "<leader>d", group = "[D]ocument" },
+        { "<leader>h", group = "Git [H]unk" },
+        { "<leader>l", group = "[L]og" },
+        { "<leader>r", group = "[R]ename" },
+        { "<leader>s", group = "[S]earch" },
+        { "<leader>w", group = "[W]orkspace" },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
@@ -387,6 +403,12 @@ require('lazy').setup({
           winblend = 20,
           mappings = {},
           file_ignore_patterns = { 'node_modules', 'yarn.lock', 'package-lock.json' },
+          layout_config = {
+            horizontal = {
+              width = 0.95,
+              preview_width = 0.4
+            },
+          }
         },
         pickers = {
           colorscheme = {
@@ -633,6 +655,13 @@ require('lazy').setup({
         },
         html = { filetypes = { 'html', 'twig', 'hbs' } },
         cssls = { filetypes = { 'css', 'scss' } },
+        angularls = {
+          -- filetypes = { 'javascript', 'typescript', 'typescriptreact', 'typescript.tsx' },
+          root_dir = require('lspconfig.util').root_pattern('angular.json', 'project.json'),
+          -- settings = {
+          --   root_dir = require('lspconfig.util').root_pattern('angular.json', 'project.json')
+          -- }
+        },
 
         lua_ls = {
           -- cmd = {...},
